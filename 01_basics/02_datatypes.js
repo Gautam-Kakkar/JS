196a970dce1cdb7d8c8bc22c22b6652063dc810e
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-06-20 11:57:37 +0000

    datatypes vid#5

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") we are using node js not browser js
+
+console.log(3+
+    3)//code readability should be good
+console.log("Gautam")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/*  DATA TYPES
+     PRIMITIVE
+number ==> 2 to pwr 53
+bigint
+string ==> "" or ''
+boolean ==> true/false
+null ==> standalone value
+undefined ==> not defined value
+symbol ==> unique
+
+
+object
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
