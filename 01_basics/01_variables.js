fd568698de11b6f534ea8092f0e658c4ba9ca56d
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-06-20 11:39:13 +0000

    variables vid#4  commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 122452
+let accountEmail= "gautam@gmail.com"
+var accountPassword = "qwert1234"
+accountCity = "Delhi" 
+let accountState;
+
+/*
+var is no longer used in js
+only let and const
+because of issue in scope
+*/
+
+
+//accountId = 2//
+
+accountEmail= "gk@gmail.com"
+accountPassword= "2121"
+accountCity= "jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
