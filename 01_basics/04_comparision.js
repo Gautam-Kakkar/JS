0f1bc6d1eaff2c8e8814c3c3c84ba6b546045e1e
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-06-25 11:36:40 +0000

    reached video 09

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,7 +1,13 @@
 //console.log(2>1);
 
-// console.log("2">1)
-// console.log("02">1)
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log("2">1)
+console.log("02">1)
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+//comparision converts null to 0 and == does not convert it to 0 
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
