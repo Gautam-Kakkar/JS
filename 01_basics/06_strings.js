714b712c54b1812d03d7d6fefc589c653b65beb9
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-07-13 14:03:55 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const name= "Gautam"
+const repoCount = 3
+
+// console.log(name +repoCount+ "Value");
+//+ contatination is outdated
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Grand Theft Auto')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
