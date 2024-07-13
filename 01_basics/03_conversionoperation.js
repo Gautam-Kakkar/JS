03ee70889030fa31e0b6b4707fb66b5ca45d5293
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-07-13 11:20:34 +0000

    revision

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = "Gautam"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-//console.log(typeof(valueInNumber));
-//console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 /* "33" ==> 33
 "33abc" ==> NaN (not a number)
@@ -14,13 +14,13 @@ false ==> 0 */
 
 let isLoggedIn = 0
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-//console.log(stringNumber);
-//console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
 
 
 ///***********OPERATIONS ***********///
