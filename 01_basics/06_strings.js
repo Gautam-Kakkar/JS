4a371755e81c8a60e81c4bb662460b8511a7a19a
Gautam-Kakkar <135862306+Gautam-Kakkar@users.noreply.github.com>
2024-07-14 13:53:05 +0000

    strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -14,3 +14,15 @@ console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(4));
+console.log(gameName.indexOf('r'));
+console.log(gameName.substring(0,5));
+console.log(gameName.slice(-12,9));
+
+const nameStringOne="      Gautam     ";
+console.log(nameStringOne);
+console.log(nameStringOne.trim());
+
+const url = "https://www.google.co.in/"
+console.log(url.replace('google','facebook'))
+
+console.log(gameName.split(' '));
